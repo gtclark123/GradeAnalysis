b97026107bcf0f3db3a2ded37a3ba2c2fdac3b47
Shannon <scgettin@asu.edu>
2019-12-06 05:32:13 -0700

    errors?

--- src/ViewFactory.java
+++ src/ViewFactory.java
@@ -3,9 +3,7 @@ public class ViewFactory {
     static public View[] createViews(Data data) {
         return new View[]{
             new DataView(data),
-            new AnalysisView(data),
-            new ErrorView(data),
-            new KeyboardView(data)
+            new AnalysisView(data)
             // OtherView(data)
             // add a new view here...
         };
